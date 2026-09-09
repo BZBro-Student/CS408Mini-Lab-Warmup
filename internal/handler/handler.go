5d36d956582722a36b256acc28c99cbb1735b4b8
BZBro-Student <brodenbenson@gmail.com>
2026-09-09 10:50:47 -0600

    Enhance code clarity with additional comments
    Added comments to clarify the purpose of code sections.

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -33,6 +33,7 @@ func HandleHome(w http.ResponseWriter, r *http.Request) {
 
 // HandleGrades serves the grades path
 func HandleGrades(w http.ResponseWriter, r *http.Request) {
+	//load .env
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println(".env failed to load")
@@ -43,13 +44,14 @@ func HandleGrades(w http.ResponseWriter, r *http.Request) {
 	if domain == "" || token == "" {
 		log.Fatal("No value assigned to the environment variables")
 	}
-
+	//establish connection to canvas
 	client := canvas.NewClient(domain, token)
 	courses, err := client.FetchFavoriteCoursesGrades(r.Context())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	//render the result of courses using Grades template
 	renderHelper(w, r, components.Grades(courses))
 }
 
@@ -71,10 +73,13 @@ func HandleProgress(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	//render the CourseProgress page with a dropdown menu and the empty class template
 	renderHelper(w, r, components.CourseProgress(courses, components.EmptyClass()))
 }
 
+//Handles loading course progress upon the dropdown menu being used
 func HandleCourseLoad(w http.ResponseWriter, r *http.Request) {
+	//changing the dropdown results in the course_id being sent out
 	courseID := r.URL.Query().Get("course_id")
 	if courseID == "" {
 		renderHelper(w, r, components.EmptyClass())
@@ -97,13 +102,13 @@ func HandleCourseLoad(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid course ID", http.StatusBadRequest)
 		return
 	}
-
+	//using course ID match pull the course modules
 	details, err := client.FetchModules(r.Context(), courseIDInt)
-
+	
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	//render just the class moudles without reloading the whole page
 	renderHelper(w, r, components.ClassProgress(details))
 }
